f8f38096d6e9d1d1503575ed5631111063a24dc7
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-01-27 02:28:20 +0100

    Update Header.jsx

--- AHK-main/client/src/components/Header.jsx
+++ AHK-main/client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/landPage.css';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
@@ -13,36 +14,15 @@ const Header = () => {
         'कार्य'     // Hindi
     ];
 
-    // Array of all dark colors
-    const colors = [
-        'var(--dark-blue)',
-        'var(--dark-red)',
-        'var(--dark-wine)',
-        'var(--dark-maroon)',
-        'var(--full-wine)',
-        'var(--full-maroon)',
-        'var(--full-orange)'
-    ];
-    
     const [indice, setIndice] = useState(0);
     const [displayedText, setDisplayedText] = useState('');
-    const [currentColor, setCurrentColor] = useState(colors[0]);
-    
-    // Function to get random color
-    const getRandomColor = () => {
-        const randomIndex = Math.floor(Math.random() * colors.length);
-        return colors[randomIndex];
-    };
-    
+
     useEffect(() => {
         const changeWord = () => {
             const nextIndex = (indice + 1) % palabras.length;
             const nextWord = palabras[nextIndex];
             let currentText = '';
             let charIndex = 0;
-
-            // Set a new random color when starting a new word
-            setCurrentColor(getRandomColor());
 
             const letterInterval = setInterval(() => {
                 currentText += nextWord[charIndex];
@@ -67,15 +47,12 @@ const Header = () => {
 
     return (
         <div className="encabezado">
-            <div className="encabezado__contenedor">                
+            <div className="imagen-container">
+                <img src="/landpage.png" className="encabezado__imagen" alt="background" />
                 <h1 className="encabezado__titulo">
                     <span 
                         className="encabezado__texto"
                         onClick={handleClick}
-                        style={{ 
-                            cursor: 'pointer',
-                            color: currentColor // Use the random color
-                        }}
                     >
                         {displayedText}
                     </span>
