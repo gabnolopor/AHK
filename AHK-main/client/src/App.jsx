9a9cd6f3e705d35721eebaf96eba83fe7816af57
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-01-27 02:36:15 +0100

    Update App.jsx

--- AHK-main/client/src/App.jsx
+++ AHK-main/client/src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandPage from './pages/LandPage';
 import BoxSelect from './pages/BoxSelect';
+import MusicPage from './pages/MusicPage';
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<LandPage />} />
         <Route path="/boxselect" element={<BoxSelect />} />
+        <Route path="/music" element={<MusicPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
