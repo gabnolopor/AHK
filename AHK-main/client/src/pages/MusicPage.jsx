4ea84521f339765869e51feb9932fbd7883a3f7a
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-01-27 02:34:33 +0100

    Add files via upload

---
+++ AHK-main/client/src/pages/MusicPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/music.css'
+import Music from '../components/Music'
+
+const MusicPage = () => {
+  return (
+    <>
+      <Music />
+    </>
+  )
+}
+
+export default MusicPage;
