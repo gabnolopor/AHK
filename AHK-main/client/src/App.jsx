5b99fbfd91d76fb190e53ce066407c4d2a51d60d
Gabino Lopez <gabinoelp20@hotmail.com>
2025-02-24 14:10:57 +0100

    Add files via upload

--- AHK-main/client/src/App.jsx
+++ AHK-main/client/src/App.jsx
@@ -1,18 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandPage from './pages/LandPage';
 import BoxSelect from './pages/BoxSelect';
+import Writings from './pages/Writings';
+import ArtRoom from './pages/ArtRoom';
+import PhotoRoom from './pages/PhotoRoom';
+import Credits from './pages/Credits';
 import MusicPage from './pages/MusicPage';
-
+import Admin from './pages/Admin';
+import PrivateRoute from './components/PrivateRoute';
+import TestApi from './components/TestApi';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandPage />} />
+        <Route path="/testapi" element={<TestApi />} />
         <Route path="/boxselect" element={<BoxSelect />} />
+        <Route path="/writings" element={<Writings />} />
+        <Route path="/artroom" element={<ArtRoom />} />
+        <Route path="/photoroom" element={<PhotoRoom />} />
+        <Route path="/credits" element={<Credits />} />
         <Route path="/music" element={<MusicPage />} />
+        <Route path="/admin/login" element={<Admin />} />
+        <Route 
+          path="/admin" 
+          element={
+            <PrivateRoute>
+              <Admin />
+            </PrivateRoute>
+          } 
+        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
