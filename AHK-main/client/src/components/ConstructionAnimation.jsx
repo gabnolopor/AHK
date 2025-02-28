3b15ef2901ae2f09a7cba110e2cdb351f6edf4cc
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-02-28 04:24:06 +0100

    Add files via upload

---
+++ AHK-main/client/src/components/ConstructionAnimation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ConstructionAnimation = () => {
+    return (
+        <lord-icon
+            src="https://cdn.lordicon.com/pflszboa.json"
+            trigger="hover"
+            speed="1000"
+            style={{ width: 50, height: 50 }}>
+        </lord-icon>
+    );
+};
+
+export default ConstructionAnimation;
