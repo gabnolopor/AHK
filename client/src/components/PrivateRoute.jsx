2354251fc3b0c9e1b81f8e7363fbaf16e47a3591
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-03-16 09:13:29 +0100

    Delete client directory

--- client/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem('isAdminAuthenticated') === 'true';
-  
-  return isAuthenticated ? children : <Navigate to="/admin/login" />;
-};
-
-export default PrivateRoute;
