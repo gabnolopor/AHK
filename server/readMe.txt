396ce4ca97b788101c7344dcf3e7aeb7b3e4929d
Judith Ríos <165655155+DidithJim@users.noreply.github.com>
2025-03-16 09:13:39 +0100

    Delete server directory

--- server/readMe.txt
+++
@@ -1,1 +0,0 @@
-Solo he subido el server y el client, el server esta en AHK-nuevo/server y el client. La parte de AKH-mobile no la he tocado por lo que puedes unirlo más tarde.
--- server/routes/BiographyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const biographyController = require('../controllers/BiographyController');
-
-router.get('/biography', biographyController.getBiography);
-router.put('/biography', biographyController.updateBiography);
-
-module.exports = router;    
